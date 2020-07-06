32f6445971907630b3149b58201571260b7ba358
凌风1205 <breeze_lfeng@163.com>
2020-07-07 07:41:23 +0800

    redis缓存入门实例

---
+++ springboot-mybatisplus-redis/src/main/java/com/clfeng/SpringbootMybatisplusRedisApplication.java
@@ -0,0 +1,13 @@
+package com.clfeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMybatisplusRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMybatisplusRedisApplication.class, args);
+    }
+
+}
---
+++ springboot-mybatisplus-redis/src/test/java/com/clfeng/SpringbootMybatisplusRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clfeng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMybatisplusRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
