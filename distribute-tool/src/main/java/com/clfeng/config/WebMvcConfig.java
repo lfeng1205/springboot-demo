6ec8093a3b413f09935c65486b12a9c9366c950b
凌风1205 <breeze_lfeng@163.com>
2020-07-06 22:59:28 +0800

    注册登录拦截器

--- distribute-tool/src/main/java/com/clfeng/config/WebMvcConfig.java
+++ distribute-tool/src/main/java/com/clfeng/config/WebMvcConfig.java
@@ -1,8 +1,15 @@
 package com.clfeng.config;
 
+import com.clfeng.interceptor.MyLoginInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new MyLoginInterceptor());
+    }
 }
