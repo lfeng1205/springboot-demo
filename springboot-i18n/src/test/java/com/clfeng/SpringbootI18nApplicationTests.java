2e1d841e89089647a1455c2d5dbad140c2ce4716
凌风1205 <breeze_lfeng@163.com>
2020-07-08 22:00:31 +0800

    Springboot国际化

---
+++ springboot-i18n/src/test/java/com/clfeng/SpringbootI18nApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clfeng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootI18nApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
